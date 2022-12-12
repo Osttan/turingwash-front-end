a647936996ccd4521d8fda274e82e14005436d8c
Andre Ostan <andre.ostan@gmail.com>
2022-12-12 20:21:00 -0300

    Gerando dinamicamente a lista de carros em meuscarros.html

---
+++ scripts/carros.js
@@ -0,0 +1,14 @@
+const carros = [
+  {
+    fabricante: 'Nissan',
+    nome: 'Kicks',
+    ano: '2018',
+    logado: true,
+  },
+  {
+    fabricante: 'Chevrolet',
+    nome: 'Onix',
+    ano: '2015',
+    logado: true,
+  },
+];
