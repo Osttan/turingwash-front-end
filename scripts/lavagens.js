a3409cba003a064085c24785fa8f6674ed936a4a
Andre Ostan <andre.ostan@gmail.com>
2022-12-12 20:33:48 -0300

    Tarefa 4 integralmente concluída

---
+++ scripts/lavagens.js
@@ -0,0 +1,26 @@
+const lavagens = [
+  {
+    lavador: 'Matheus Oliveira',
+    carro: 'Chevrolet Onix (2015)',
+    valorCobrado: 75,
+    data: '2022-05-29',
+  },
+  {
+    lavador: 'Joana Santos',
+    carro: 'Nissan Kicks (2018)',
+    valorCobrado: 85,
+    data: '2022-04-20',
+  },
+  {
+    lavador: 'Pedro Sampaio',
+    carro: 'Hyundai HB20 (2020)',
+    valorCobrado: 65,
+    data: '2022-08-01',
+  },
+  {
+    lavador: 'John Doe',
+    carro: 'Chevrolet Cruze (2017)',
+    valorCobrado: 80,
+    data: '2022-06-03',
+  },
+];
